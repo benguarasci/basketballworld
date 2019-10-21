f906d7965f9a5a9deef1829d733b628293eccb0c
Eli Brooks <elibrks@gmail.com>
2019-10-21 13:40:53 -0700

    added broken account creation code

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -7,4 +7,13 @@ router.get('/', (req, res, next) => {
     res.render('account');
 });
 
+/* POST for account registration. */
+router.post('/account', function(req,res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const pw = req.body.pw;
+    const pw2 = req.body.pw2;
+
+});
+
 module.exports = router;
