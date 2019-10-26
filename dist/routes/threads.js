0d66ca5a9c10070a04adeede54c5a362a607395b
Eli Brooks <elibrks@gmail.com>
2019-10-25 20:57:52 -0700

    added threads to create and view

--- dist/routes/threads.js
+++ dist/routes/threads.js
@@ -8,9 +8,49 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = __importStar(require("express"));
+var DbClient = require("../DbClient");
 var router = express.Router();
-/* GET threads page. */
-router.get('/', function (req, res, next) {
-    res.render('threads');
+/* GET view page. */
+router.get('/view', function (req, res, next) {
+    DbClient.connect()
+        .then(function (db) {
+        return db.collection("threads").find().toArray();
+        //let myJSON = JSON.stringify(threads);
+        //res.render('threads/view' , {title: "hi", content: "hello"});
+    })
+        .then(function (result) {
+        console.log(result);
+        var myJSON = JSON.stringify(result);
+        res.render('threads/view', { title: myJSON, content: "hi" });
+    })
+        .catch(function (err) {
+        console.log(err.message);
+    });
+});
+router.get('/create', function (req, res, next) {
+    res.render('threads/create');
+});
+router.post('/create', function (req, res) {
+    var title = req.body.title;
+    var content = req.body.content;
+    DbClient.connect()
+        .then(function (db) {
+        return db.collection("threads").insertOne({ title: title, content: content });
+    })
+        .catch(function (err) {
+        console.log(err.message);
+    });
+    DbClient.connect()
+        .then(function (db) {
+        return db.collection("threads").find().toArray();
+    })
+        .then(function (result) {
+        console.log(result);
+        res.send(result);
+    })
+        .catch(function (err) {
+        console.log(err.message);
+    });
+    res.render('threads/create');
 });
 module.exports = router;
--- src/routes/threads.ts
+++ src/routes/threads.ts
@@ -1,10 +1,61 @@
 import * as express from "express";
+import DbClient = require("../DbClient");
+
 let router = express.Router();
 
-/* GET threads page. */
 
-router.get('/', (req, res, next) => {
-    res.render('threads');
+/* GET view page. */
+router.get('/view', (req, res, next) => {
+    DbClient.connect()
+        .then((db: any) => {
+            return db!.collection("threads").find().toArray();
+            //let myJSON = JSON.stringify(threads);
+
+            //res.render('threads/view' , {title: "hi", content: "hello"});
+
+        })
+        .then((result:any) => {
+            console.log(result);
+            let myJSON = JSON.stringify(result);
+            res.render('threads/view' , {title: myJSON, content: "hi"});
+
+        })
+        .catch((err: any) => {
+            console.log(err.message);
+        });
+
+});
+
+router.get('/create', (req, res, next) => {
+    res.render('threads/create');
+});
+
+router.post('/create', (req, res)=>{
+    let title = req.body.title;
+    let content = req.body.content;
+
+    DbClient.connect()
+        .then((db: any) => {
+            return db.collection("threads").insertOne({title: title, content: content});
+        })
+        .catch((err: any) => {
+            console.log(err.message);
+        });
+
+    DbClient.connect()
+        .then((db: any) => {
+            return db!.collection("threads").find().toArray();
+        })
+        .then((result:any) => {
+            console.log(result);
+            res.send(result);
+        })
+        .catch((err: any) => {
+            console.log(err.message);
+        });
+
+    res.render('threads/create');
+
 });
 
 module.exports = router;
