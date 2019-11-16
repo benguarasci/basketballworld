36374c96c0a332c8c505ae0afd1bf30e6c88a617
Cole Dodd <coledodd97@gmail,com>
2019-11-16 06:54:31 -0800

    added pages for each thread

--- dist/routes/threads.js
+++ dist/routes/threads.js
@@ -3,21 +3,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var DbClient = require("../DbClient");
 var router = express_1.Router();
+var ObjectId = require("mongodb").ObjectID;
 router.get("/", function (req, res) {
     DbClient.connect()
         .then(function (db) { return db.collection("threads").find().toArray(); })
         .then(function (arr) {
-        var links = arr.map(function (thread) { return "/threads/" + thread._id.toString() + ":thread"; });
-        res.render("placeholders/threads", { threads: arr, links: links, });
+        var links = arr.map(function (thread) { return "/threads/" + thread._id.toString(); });
+        res.render("placeholders/threads", { 'username': req.cookies.username, threads: arr, links: links, });
     }).catch(function (err) {
         console.log(err.message);
     });
 });
-router.get("/:id", function (req, res) {
-    res.send("congratulations");
+router.get("/:thread", function (req, res) {
+    var threadID = new ObjectId(req.params.thread);
+    var thisDB;
+    DbClient.connect()
+        .then(function (db) {
+        thisDB = db;
+        return db.collection("threads").findOne({ _id: threadID });
+    }).then(function (thread) {
+        if (thread === null)
+            res.render("placeholders/homepage", { "message": "couldn't find page, sorry" });
+        else {
+            thisDB.collection("posts").find({ parentThread: threadID }).toArray()
+                .then(function (arr) {
+                var posts;
+                if (arr.length === 0)
+                    posts = [];
+                else
+                    posts = arr;
+                res.render("placeholders/thread", { username: req.cookies.username, thread: thread, posts: posts, link: "/threads/" + req.params.thread });
+            }).catch(function (err) { console.log(err.message); });
+        }
+    }).catch(function (err) { console.log(err.message); });
+});
+router.post(":thread", function (req, res) {
 });
 router.get("/create", function (req, res) {
-    res.render("placeholders/create_threads");
+    res.render("placeholders/create_threads", { 'username': req.cookies.username });
 });
 router.post("/create", function (req, res) {
     if (!("username" in req.cookies)) {
@@ -27,7 +50,7 @@ router.post("/create", function (req, res) {
     var title = req.body.title;
     var desc = req.body.description;
     if (title === "" || desc == "") {
-        res.render("placeholders/create_threads", { "message": "please complete all inputs" });
+        res.render("placeholders/create_threads", { 'username': req.cookies.username, "message": "please complete all inputs" });
         return;
     }
     var owner = req.cookies.username;
@@ -36,7 +59,7 @@ router.post("/create", function (req, res) {
     var ms = d.getTime();
     var count = 0;
     DbClient.connect()
-        .then(function (db) { return db.collection("threads").insertOne({ title: title, description: desc, owner: owner, date: date, ms: ms, count: count, last: date, by: owner }); })
+        .then(function (db) { return db.collection("threads").insertOne({ 'username': req.cookies.username, title: title, description: desc, owner: owner, date: date, ms: ms, count: count, last: date, by: owner }); })
         .then(function (id) { console.log(id); })
         .catch(function (err) { console.log(err); });
 });
--- src/routes/threads.ts
+++ src/routes/threads.ts
@@ -1,32 +1,61 @@
 import {Request, Response, NextFunction, Router} from "express";
 const DbClient = require("../DbClient");
 const router = Router();
-
+const ObjectId = require("mongodb").ObjectID;
 router.get("/", (req : Request, res : Response) => {
     DbClient.connect()
         .then((db: any) => db!.collection("threads").find().toArray())
         .then((arr:any) => {
-            let links = arr.map((thread : any) => "/threads/"+thread._id.toString()+":thread");
-            res.render("placeholders/threads", {threads : arr, links : links, });
+            let links = arr.map((thread : any) => "/threads/"+thread._id.toString());
+            res.render("placeholders/threads", {'username':req.cookies.username, threads : arr, links : links, });
         }).catch ((err: any) => {
             console.log(err.message);
         })
 });
-router.get("/:id", (req: Request, res: Response) => {
-    res.send("congratulations");
+router.get("/:thread", (req: Request, res: Response) => {
+    let threadID = new ObjectId(req.params.thread);
+    let thisDB : any;
+    DbClient.connect()
+        .then ((db : any) => {
+            thisDB = db;
+            return db!.collection("threads").findOne({_id : threadID})
+        }).then ((thread : any) => {
+            if (thread === null) res.render("placeholders/homepage", {"message":"couldn't find page, sorry"});
+            else {
+                thisDB!.collection("posts").find({parentThread: threadID}).toArray()
+                    .then((arr:any)=>{
+                        let posts;
+                        if (arr.length === 0) posts = [];
+                        else posts = arr;
+
+                        res.render("placeholders/thread", {username:req.cookies.username, thread:thread, posts:posts, link:"/threads/"+req.params.thread})
+                    }).catch((err:any)=>{console.log(err.message)})
+            }
+        }).catch((err:any)=>{console.log(err.message)})
 });
+// router.post(":thread", (req: Request, res: Response) => {
+//     if (!("username" in req.cookies)) {
+//         res.render("placeholders/threads", {"message": "you are not logged in"});
+//         return;
+//     }
+//
+// });
 router.get("/create", (req : Request, res : Response) => {
-    res.render("placeholders/create_threads");
+    if (!("username" in req.cookies)) {
+        res.render("placeholders/threads", {"message": "you are not logged in"});
+        return;
+    }
+    res.render("placeholders/create_threads", {'username':req.cookies.username});
 });
 router.post("/create", (req: Request, res: Response) => {
     if (!("username" in req.cookies)) {
-        res.render("placeholders/create_threads", {"message": "you are not logged in"});
+        res.render("placeholders/threads", {"message": "you are not logged in"});
         return;
     }
     let title = req.body.title;
     let desc = req.body.description;
     if (title === "" || desc == "") {
-        res.render("placeholders/create_threads", {"message": "please complete all inputs"});
+        res.render("placeholders/create_threads", {'username':req.cookies.username, "message": "please complete all inputs"});
         return;
     }
     let owner = req.cookies.username;
@@ -35,7 +64,7 @@ router.post("/create", (req: Request, res: Response) => {
     let ms = d.getTime();
     let count = 0;
     DbClient.connect()
-        .then ((db : any) => db!.collection("threads").insertOne({title: title, description: desc, owner: owner, date: date, ms: ms, count: count, last: date, by:owner}))
+        .then ((db : any) => db!.collection("threads").insertOne({'username':req.cookies.username, title: title, description: desc, owner: owner, date: date, ms: ms, count: count, last: date, by:owner}))
         .then ((id : any) => {console.log(id)})
         .catch((err: any) => {console.log(err)});
 });
