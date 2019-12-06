a49b3b3c58ff7aedf9a78a3a23f9539fd957a693
iyrtitl <kallio@uvic.ca>
2019-12-05 16:15:52 -0800

    travis

--- src/test/sanity.test.ts
+++ src/test/sanity.test.ts
@@ -1,10 +1,8 @@
 // https://mochajs.org/#getting-started
 
 import {database, usersCol} from "../app";
+let app = require('../app');
 
-beforeEach(async function() {
-    while(database == null) { }
-});
 
 describe('#FindOneUser', function() {
     it('Check if at least one user exists in the database.', async function() {
