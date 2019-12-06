696de512f64442fc4d284c2ae5d86b4829d48257
iyrtitl <kallio@uvic.ca>
2019-12-05 19:07:02 -0800

    travis

--- src/test/sanity.test.ts
+++ src/test/sanity.test.ts
@@ -1,10 +1,24 @@
 // https://mochajs.org/#getting-started
 
-import {database, usersCol} from "../app";
-let app = require('../app');
+import {MongoClient} from "mongodb";
 
+describe('User Test(s)', function() {
 
-describe('#FindOneUser', function() {
+    let usersCol: any;
+    let database: any;
+    const uri = "mongodb+srv://admin:m39dDRPEHac6UCWj@3-2-fjpaq.gcp.mongodb.net/test";
+
+    before(async function() {
+        MongoClient.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true }, (error: any, client: any) => {
+            if(error) {
+                throw error;
+            }
+            database = client.db("bbworld_test");
+            usersCol = database.collection("users");
+            console.log("Connected to `" + "bbworld_test" + "`!");
+        });
+    });
+
     it('Check if at least one user exists in the database.', async function() {
         const users = await usersCol.findOne();
         users.should.have.equals(1);
