21132483d94d8a0633a969c015f1c6701a0e92a4
Eli Brooks <elibrks@gmail.com>
2019-12-06 19:55:42 -0800

    added req res base classes for tests

--- src/test/helper.test.ts
+++ src/test/helper.test.ts
@@ -4,3 +4,26 @@ before(async() => {
     await DbClient.connect("bbworld_test");
     console.log("Connected to database: 'bbworld_test'\n");
 });
+
+export class MockReq {
+    public body : any;
+    public cookies : any;
+    public params : any;
+    constructor() {
+        this.body = null;
+        this.cookies = null;
+        this.params = null;
+    }
+    setUserName(name:string) {
+        this.cookies = {username:name}
+    }
+}
+export class MockRes {
+    public options : any;
+    constructor() {
+        this.options = null;
+    }
+    render( pug : string, options : any) {
+        this.options = options;
+    }
+}
--- src/test/my.test.ts
+++ src/test/my.test.ts
@@ -2,33 +2,13 @@ import {MongoClient} from "mongodb";
 import {assert} from 'chai';
 const DbClient = require("../DbClient");
 import {isBanned, isBannedBy_account, isAdmin, isAdmin_render, canModify, canModify_Thread} from "../managers/activityHandling";
+import { MockReq } from "./helper.test";
+import { MockRes } from "./helper.test";
 
-class Request {
-    public body : any;
-    public cookies : any;
-    public params : any;
-    constructor() {
-        this.body = null;
-        this.cookies = null;
-        this.params = null;
-    }
-    setUserName(name:string) {
-        this.cookies = {username:name}
-    }
-}
-class Response {
-    public options : any;
-    constructor() {
-        this.options = null;
-    }
-    render( pug : string, options : any) {
-        this.options = options;
-    }
-}
 describe("Activity Handling Tests", ()=>{
     it ("user is not banned", async ()=>{
-        let req = new Request();
-        let res = new Response();
+        let req = new MockReq();
+        let res = new MockRes();
         req.setUserName("root");
         //console.log("is Banned:" + await isBanned(req, res));
         assert(await isBanned(req, res) === false);
@@ -39,8 +19,8 @@ describe("Activity Handling Tests", ()=>{
     it ("user is banned", async ()=>{
         //let db = await DbClient.connect();
         let feedback = await DbClient.usersCol.insertOne({name:"jerry", email:"jerry's email", pw:"jerry's password", level:0});
-        let req = new Request();
-        let res = new Response();
+        let req = new MockReq();
+        let res = new MockRes();
         req.setUserName("jerry");
         assert(await isBanned(req, res) === true);
         assert((await isBannedBy_account("jerry", res)) === true);
@@ -50,8 +30,8 @@ describe("Activity Handling Tests", ()=>{
     });
     it ("testing modifying rights", async () =>{
         let feedback = await DbClient.threadsCol.insertOne({author:"root"});
-        let req = new Request();
-        let res = new Response();
+        let req = new MockReq();
+        let res = new MockRes();
         req.setUserName("root");
         let bool = await canModify_Thread(feedback.insertedId, req, res)
         assert(bool);
