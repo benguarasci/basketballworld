12dceaec8f8367f82696519cfc453029136af06d
Eli Brooks <elibrks@gmail.com>
2019-12-06 23:17:07 -0800

    ts-lint changes for routes/threads

--- src/routes/threads.ts
+++ src/routes/threads.ts
@@ -3,7 +3,6 @@ import {isLoggedIn, isLoggedIn_NoRender, pushTag, retrieveProfile} from "../mana
 const DbClient = require("../DbClient");
 const router = Router();
 const ObjectId = require("mongodb").ObjectID;
-//import {postsCol} from "../app";
 import createPostForm from "../mymodels/createPost";
 import {isBanned, isAdmin, isBannedBy_account, canModify, canModify_Thread, canModify_Post} from "../managers/activityHandling";
 import {
@@ -14,143 +13,155 @@ import {
     retrieveTaggedThreads,
     retrieveMyThreads,
     findAllPosts,
-    getThread
+    getThread,
 } from "../managers/thread";
 
-router.get("/view", (req : Request, res : Response) => {
+router.get("/view", (req: Request, res: Response) => {
     listThreads()
-        .then((threads:any) => {
-            res.render("threads/view", {'user':req.cookies.username, threads : threads[0], links : threads[1]});
-        })
+        .then((threads: any) => {
+            res.render("threads/view", {user: req.cookies.username, threads: threads[0], links: threads[1]});
+        });
 });
 
-router.get("/create", (req : Request, res : Response) => {
+router.get("/create", (req: Request, res: Response) => {
     isBanned(req, res)
-        .then((bool: boolean)=>{
-            if (!bool)
-                res.render("threads/create", {'user':req.cookies.username});
-        })
+        .then((bool: boolean) => {
+            if (!bool) {
+                res.render("threads/create", {user: req.cookies.username});
+            }
+        });
 });
 
 router.post("/create", async (req: Request, res: Response) => {
     if (!(await isBanned(req, res))) {
-            await createThread(req, res).then((id : any) => {res.redirect("/threads/"+id.insertedId.toString())});
-
-        }
+        await createThread(req, res).then((id: any) => {
+            res.redirect("/threads/" + id.insertedId.toString());
+        });
+    }
 });
 
 router.post("/delete/:id", async (req: Request, res: Response) => {
-    if (await deleteThread(req, res)) res.redirect('/profile/home');
+    if (await deleteThread(req, res)) {
+        res.redirect("/profile/home");
+    }
 });
 
 router.post("/edit/:id", async (req: Request, res: Response) => {
-    let threadID = new ObjectId(req.params.id);
+    const threadID = new ObjectId(req.params.id);
 
-    if (await canModify_Thread(ObjectId(req.params.id), req, res))
+    if (await canModify_Thread(ObjectId(req.params.id), req, res)) {
         res.render("threads/edit", {
-            'user':req.cookies.username,
-            thread: await getThread(threadID).catch((err: any) => console.log(err))
+            thread: await getThread(threadID).catch((err: any) => console.log(err)),
+            user: req.cookies.username,
         });
+    }
 });
 
 router.post("/confirm", async (req: Request, res: Response) => {
-
     await editThread(req, res);
-    res.redirect('/profile/home');
+    res.redirect("/profile/home");
 });
 
 router.get("/editPost/:id", (req: Request, res: Response) => {
-    DbClient.postsCol.findOne({_id:ObjectId(req.params.id)})
-        .then((post : any)=>{
+    DbClient.postsCol.findOne({_id: ObjectId(req.params.id)})
+        .then((post: any) => {
             if (post === null) {
                 res.render("index", {
-                    "message": "post does not exist",
-                    "user": req.cookies.username
+                    message: "post does not exist",
+                    user: req.cookies.username,
                 });
                 return;
             }
             canModify(post, req, res)
-                .then((bool:boolean)=> {
+                .then((bool: boolean) => {
                     if (bool) {
-                        res.render("threads/editPost", {"user": req.cookies.username, post: post})
+                        res.render("threads/editPost", {user: req.cookies.username, post: {post}});
                     }
-                })
+                });
         });
 });
 router.post("/editPost/:parent/:id", (req: Request, res: Response) => {
     canModify_Post(ObjectId(req.params.id), req, res)
         .then((bool: boolean) => {
-            if (bool)
+            if (bool) {
                 DbClient.postsCol.updateOne({_id: ObjectId(req.params.id)}, {$set: {content: req.body.content}})
-                    .then(()=>{res.redirect("/threads/"+req.params.parent)})
-        })
+                    .then(() => {
+                        res.redirect("/threads/" + req.params.parent);
+                    });
+            }
+        });
 });
 router.get("/editThread/:id", (req: Request, res: Response) => {
-    console.log(req.params.id);
-    console.log("lilypad");
     canModify_Thread(req.params.id, req, res)
         .then((bool: boolean) => {
-            console.log("tree people");
             if (bool) {
-                console.log("johny depp");
-                console.log(req.params.id);
                 DbClient.threadsCol.findOne({_id: ObjectId(req.params.id)})
                     .then((thread: any) => {
-                        if (thread === null) res.render("index", {
-                            "message": "thread does not exist",
-                            "user": req.cookies.username
-                        });
-                        res.render("threads/edit", {"user": req.cookies.username, thread: thread})
-                    })
-            } else {
-                console.log("baby pie");
+                        if (thread === null) {
+                            res.render("index", {
+                                message: "thread does not exist",
+                                user: req.cookies.username,
+                            });
+                        }
+                        res.render("threads/edit", {user: req.cookies.username, thread: {thread}});
+                    });
             }
-        })
+        });
 });
 
 router.get("/:thread", (req: Request, res: Response) => {
     getThread(req.params.thread)
-        .then((thread:any)=>{
-            if (thread === null) res.render("index", {"message":"couldn't find page, sorry"});
-            else {
+        .then((thread: any) => {
+            if (thread === null) {
+                res.render("index", { message: "couldn't find page, sorry" });
+            } else {
                 isAdmin(req)
-                    .then((amAdmin:boolean)=> {
-                        let thisIsMine : boolean;
+                    .then((amAdmin: boolean) => {
+                        let thisIsMine: boolean;
                         thisIsMine = amAdmin || req.cookies.username === thread.author;
                         findAllPosts(req.params.thread)
-                            .then((posts)=>{
+                            .then((posts) => {
                                 let isMine: any;
-                                if (!amAdmin) isMine = posts.map((post : any)=> (post.author == req.cookies.username));
-                                else isMine = posts.map((post : any)=> true);
-                                res.render("threads/thread", {    user:req.cookies.username,
-                                    thread:thread,
-                                    posts:posts,
-                                    target:"/threads/"+req.params.thread,
-                                    isMine: isMine,
-                                    thisIsMine: thisIsMine
-                                })
-                            })
+                                if (!amAdmin) {
+                                    isMine = posts.map((post: any) => (post.author === req.cookies.username));
+                                } else {
+                                    isMine = posts.map((post: any) => true);
+                                }
+                                res.render("threads/thread", {
+                                    isMine: {isMine},
+                                    posts: {posts},
+                                    target: "/threads/" + req.params.thread,
+                                    thisIsMine: {thisIsMine},
+                                    thread: {thread},
+                                    user: req.cookies.username,
+                                });
+                            });
                     });
             }
-        })
+        });
 });
 
 router.post("/:thread",  (req: Request, res: Response) => {
     isBanned(req, res)
-        .then((bool:boolean)=>{
+        .then((bool: boolean) => {
             if (!bool) {
-                if (!isLoggedIn_NoRender(req, res)) return;
-                let new_post = new createPostForm(req);
-                if (new_post.isFormComplete(res)) {
-                    DbClient.postsCol.insertOne({content:new_post.content, author:new_post.author,parentThread:new_post.parentThread, date:new_post.date, ms:new_post.ms})
-                        .then(()=>{
-                            res.redirect("/threads/"+req.params.thread);
-                        })
+                if (!isLoggedIn_NoRender(req, res)) {
+                    return;
+                }
+                const newPost = new createPostForm(req);
+                if (newPost.isFormComplete(res)) {
+                    DbClient.postsCol.insertOne({
+                        author: newPost.author,
+                        content: newPost.content,
+                        date: newPost.date,
+                        ms: newPost.ms,
+                        parentThread: newPost.parentThread,
+                    }).then(() => {
+                            res.redirect("/threads/" + req.params.thread);
+                        });
                 }
             }
-        })
+        });
 });
-
-
 
 module.exports = router;
